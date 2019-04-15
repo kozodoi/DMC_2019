ba7e5ee6dead3f0106333e2bcd8c5c89110069f1
Nikita Kozodoi <kozodoin4.hub@rdc.wiwi.hu-berlin.de>
2019-04-15 13:39:00 +0200

    try to use weighted logloss as objective

--- codes/functions.py
+++ codes/functions.py
@@ -13,6 +13,7 @@ def prediction_reward(y_true, y_preds):
         return 'profit', costs, True
     
     
+    
 ########### FUNCTION FOR PROFIT MEASURE WITH CUTOFF PARAMETER
 
 # can use after modeling for threshold optimization
@@ -27,6 +28,27 @@ def recompute_reward(y_true, y_preds, cutoff = 0.5):
     
     
     
+########### FUNCTION FOR WEIGHTED LOG-LOSS OBJECTIVE
+
+# false positive is 5 times worse than false negative
+# logsloss for LGB that penalizes false positives more severly
+    
+def weighted_logloss_train(y_true, y_preds):
+    beta = 5
+    p = 1. / (1. + np.exp(-y_preds))
+    y = y_true
+    grad = p * (beta + y - beta*y) - y
+    hess = p * (1 - p) * (beta + y - beta*y)
+    return grad, hess
+
+def weighted_logloss_eval(y_true, y_preds): 
+    beta = 5
+    p = 1. / (1. + np.exp(-y_preds))
+    y = y_true
+    val = -y * np.log(p) - beta * (1 - y) * np.log(1 - p)
+    return 'weighted logloss', np.sum(val), False
+    
+
     
 ########### FUNCTION FOR COUNTING MISSINGS
 
