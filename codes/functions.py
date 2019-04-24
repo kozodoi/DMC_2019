90c1607a10949d82cce7797f24b33a4736338cb8
Nikita Kozodoi <kozodoin4.hub@rdc.wiwi.hu-berlin.de>
2019-04-24 12:19:39 +0200

    CV-based mean target encoding (does not improve profit)

--- codes/functions.py
+++ codes/functions.py
@@ -62,3 +62,74 @@ def count_missings(data):
     table = pd.concat([total, percent], axis = 1, keys = ["Total", "Percent"])
     table = table[table["Total"] > 0]
     return table
+
+
+
+
+
+##### FUNCTION FOR MEAN TARGET ENCODING 
+
+# replaces factors with mean target values per value
+# training data: encoding using internal CV
+# validation and test data: encoding using  training data
+
+def mean_target_encoding(train, valid, test, features, target, folds = 5):
+
+    ##### TRAINING
+
+    # cross-validation
+    skf = StratifiedKFold(n_splits = folds, random_state = 777, shuffle = True)
+    for n_fold, (trn_idx, val_idx) in enumerate(skf.split(train, train[target])):
+
+        # partition folds
+        trn_x, trn_y = train.iloc[trn_idx], y.iloc[trn_idx]
+        val_x, val_y = train.iloc[val_idx], y.iloc[val_idx]
+
+        # loop for facrtors
+        for var in features:
+
+            # feature name
+            name = "_".join(["mean_target_per", str(var)])
+
+            # compute means
+            means = val_x[var].map(trn_x.groupby(var)[target].mean())
+            val_x[name] = means
+
+            # impute means
+            if n_fold == 0:
+                train[name] = np.nan
+                train.iloc[val_idx] = val_x
+            else:
+                train.iloc[val_idx] = val_x
+
+
+    ##### VALIDATION
+
+    # loop for factors
+    for var in features:
+        means = valid[var].map(train.groupby(var)[target].mean())
+        valid[name] = means
+
+
+    ##### TEST
+    
+    # copy data
+    tmp_test = test.copy()
+
+    # loop for factors
+    for var in features:
+        means = tmp_test[var].map(train.groupby(var)[target].mean())
+        tmp_test[name] = means
+        
+        
+    ##### CORRECTIONS
+
+    # remove target
+    del train[target], valid[target]
+    
+    # remove factors
+    for var in features:
+        del train[var], valid[var], tmp_test[var]
+
+    # return data
+    return train, valid, tmp_test
